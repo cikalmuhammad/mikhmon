d4ca4728a981cd48b15deb6e8e2a9ae168c1a398
Muhammad Anwar <129497762+cikalmuhammad@users.noreply.github.com>
2024-11-17 17:21:37 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,11 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<21','mikhmon>|>amI=');
+
+
+$data['nk.net'] = array ('1'=>'nk.net!172.16.3.1:8800','nk.net@|@ipcbl','nk.net#|#jHGarZ9kaWFnamFiag==','nk.net%nk.net','nk.net^nk.net','nk.net&Rp','nk.net*10','nk.net(1','nk.net)','nk.net=10','nk.net@!@enable');
+
+
+$data['onein'] = array ('1'=>'onein!103.171.165.150:8800','onein@|@ipcbl','onein#|#jHGarZ9kaWFnamFiag==','onein%onein.net','onein^onein.net','onein&Rp','onein*10','onein(1','onein)','onein=10','onein@!@enable');
+$data['Pangalengan'] = array ('1'=>'Pangalengan!remote4.vpnmurahjogja.my.id:3289','Pangalengan@|@pangalengan','Pangalengan#|#aWdjaWli','Pangalengan%Pangalengan','Pangalengan^Pangalengan','Pangalengan&Rp','Pangalengan*10','Pangalengan(1','Pangalengan)','Pangalengan=10','Pangalengan@!@disable');
+$data['katapang'] = array ('1'=>'katapang!remote.vpnmurahjogja.my.id:3271','katapang@|@ipkatapang','katapang#|#jHGarZ9kaWFnamFiag==','katapang%katapang','katapang^katapang','katapang&Rp','katapang*10','katapang(1','katapang)','katapang=10','katapang@!@disable');
